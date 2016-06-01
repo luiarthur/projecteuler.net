f443d7262c5f6cf3641b5d4708de5b061cc7baec
Arthur Lui <luiarthur@gmail.com>
2016-05-31 17:27:55 -0700

    yes

--- 002/fib.scala
+++ 002/fib.scala
@@ -3,13 +3,9 @@
  * exceed four million, find the sum of the even-valued terms.
  */
 
-def buildFib(curr: Int, prev: Int, B: Int, acc: Int=0): Int = {
-  if (prev + curr > B) acc
-  else {
-    //buildFib(curr + prev, curr, B, if (curr % 2 ==0) acc+curr+prev else acc) 
-    buildFib(curr + prev, curr, B, acc+curr+prev) 
-  }
+def sumEvenFib(curr: Int, prev: Int, B: Int, acc: Int=0): Int = {
+  if (curr > B) acc
+  else sumEvenFib(curr + prev, curr, B, if (curr % 2 == 0) acc + curr else acc) 
 }
 
-//buildFib(1,0,4000000) // this is the sum of all fibs < 4000000. Now I need one that just sums the evens.
-buildFib(1,0,4) // this is the sum of all fibs < 4000000. Now I need one that just sums the evens.
+sumEvenFib(1,0,4000000) // = 4613732
