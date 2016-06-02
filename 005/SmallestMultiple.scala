0bf06718d66782609ed5247c8018423ca0c4b36e
Arthur Lui <luiarthur@gmail.com>
2016-06-01 17:01:01 -0700

    yes

---
+++ 005/SmallestMultiple.scala
@@ -0,0 +1,19 @@
+/** Smallest Multiple
+ *  2520 is the smallest number that can be divided by each of the numbers from
+ *  1 to 10 without any remainder.  What is the smallest positive number
+ *  that is evenly divisible by all of the numbers from 1 to 20?
+ */
+
+def gcd(a: Int, b: Int): Int = if (b==0) a else gcd(b, a%b)
+
+def lcm(a: Int, b: Int, c: Int = 1): Int = {
+  val g = gcd(a,b)
+  if ( g == 1 ) a*b*c else lcm(a/g, b/g, g)
+}
+
+def leastMultiple(n: Int): Int = {
+  (1 to n).foldLeft(0)((x,y) => lcm(x,y))
+}
+
+leastMultiple(10) // ?
+
